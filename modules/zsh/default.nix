4e8f64bb661793db7271ec697781a6ac311d56f6
Iohannes Mboumba <iohannes.mboumba@protonmail.com>
2025-03-29 23:09:48 +0100

    sa

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -19,7 +19,7 @@ in {
       shellAliases = {
         ll = "ls -l";
         edit = "sudo -e";
-        update = "nixos-rebuild switch --flake $NIXOS_CONFIG_DIR --fast";
+        update = "sudo nixos-rebuild switch --flake $NIXOS_CONFIG_DIR";
         clean-old = "sudo nix-collect-garbage --delete-old";
       };
 
